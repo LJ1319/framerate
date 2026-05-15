0b5e42d27810f7083991f22405d8a2ced9be044e
LJ1319 <luka.jikia.2@btu.edu.ge>
2026-05-16 03:27:21 +0400

    add test helpers and macros

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup></script>
 
 <template>
-    <img alt="framerate logo" src="/assets/framerate-analog.svg" />
+    <img alt="framerate logo" src="/assets/framerate-negative.svg" />
 </template>
